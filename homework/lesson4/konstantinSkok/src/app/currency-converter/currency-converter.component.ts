42d4afd6bc641d732c19cbc6cd1357b6c3f2d2ef
konstantin.skok <skok.konstantin@gmail.com>
2016-11-05 17:48:42 +0300

    add enum

--- homework/lesson4/konstantinSkok/src/app/currency-converter/currency-converter.component.ts
+++ homework/lesson4/konstantinSkok/src/app/currency-converter/currency-converter.component.ts
@@ -1,29 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import {Currency, CurrencyType} from '../currency';
 
 @Component({
   selector: 'app-currency-converter',
   templateUrl: './currency-converter.component.html',
   styleUrls: ['./currency-converter.component.css'],
-  //TODO: component level
   providers : [Currency]
 })
 
-//TODO: OnInit
-export class CurrencyConverterComponent implements OnInit {
-  public error: string;
-  public currency: string[] = [];
+enum convertationType {
+  Static,
+  Dinamic
+}
+
+enum convertationDirect {
+  FromCurrencyToRuble,
+  FromRubleToCurrency
+}
+
+export class CurrencyConverterComponent {
+  public staticCurrencyRate: CurrencyType[];
+  public convertationTypes: convertationType[] = [convertationType.Dinamic, convertationType.Static];
 
   constructor(private _currency: Currency) {
-    //TODO: format
-    for (let currency in _currency.staticCurrencyRate) {
-      this.currency.push(currency);
-    };
+    this.staticCurrencyRate = _currency.staticCurrencyRate;
   }
 
-  ngOnInit() {}
-
-  //TODO: forms
+  // TODO: forms
   public convert (form: HTMLFormElement) {
 
     let rateType: string = form.elements['rateType'].value;
@@ -32,15 +35,15 @@ export class CurrencyConverterComponent implements OnInit {
     let rubleSum: number = +form.elements['rubleSum'].value.trim() || 0;
     let convertType: string = form.elements['convertType'].value;
 
-    //TODO: type constants
+    // TODO: type constants
     if (rateType === 'STATIC') {
       doConvertation(this._currency.staticCurrencyRate);
     } else {
-      //TODO: subscribe => async pipe
-      this._currency.getCurrencyRate().subscribe((currency: CurrencyType) => doConvertation(currency));
+      // //TODO: subscribe => async pipe
+      // this._currency.getCurrencyRate().subscribe((currency: CurrencyType) => doConvertation(currency));
     }
 
-    function doConvertation(rate: CurrencyType) {
+    function doConvertation(rate: CurrencyType[]) {
       if (convertType === 'CR') { // currency -> rub
         form.elements['rubleSum'].value = (currencySum * rate[currency]).toFixed(2);
       } else if (convertType === 'RC') { //// rub -> currency
