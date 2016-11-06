635dc7d3ab503e0aa7874009ac7e2dc1a0359f34
konstantin.skok <skok.konstantin@gmail.com>
2016-11-06 21:10:50 +0300

    add async pipe and change result to Observable

--- homework/lesson4/konstantinSkok/src/app/currency.ts
+++ homework/lesson4/konstantinSkok/src/app/currency.ts
@@ -22,11 +22,23 @@ export class Currency {
 
   constructor(private _http: Http) {}
 
-  public  getCurrencyRate(): Observable<any> {
+  public  getCurrencyRate(): Observable<CurrencyType[]> {
     return this._http
       .get(`https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=
         +"USDRUB,EURRUB"&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=`)
-      .map((response) => response.json())
-      //.map((json) => { return {USD: json.query.results.rate['0'].Rate, EUR: json.query.results.rate[1].Rate}; } );
+      .map((response) => {
+        let json = response.json();
+        return [
+          {
+            currency: 'USD',
+            rate: <number>json.query.results.rate[0].Rate
+          },
+          {
+            currency: 'EUR',
+            rate: <number>json.query.results.rate[1].Rate
+          }
+        ];
+
+      });
   }
 }
